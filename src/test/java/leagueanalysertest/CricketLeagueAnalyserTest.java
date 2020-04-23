fe269ec9c7f976f63ab7be429ce1d7f306f785ab
aayush-aryan <log2aayusharyan@gmail.com>
2020-04-24 01:53:43 +0530

    [Aayush]added.  added  test case for cricketers having great bowling average with best striking rate

--- src/test/java/leagueanalysertest/CricketLeagueAnalyserTest.java
+++ src/test/java/leagueanalysertest/CricketLeagueAnalyserTest.java
@@ -49,25 +49,14 @@ public class CricketLeagueAnalyserTest {
         }
     }
     @Test
-    public void givenCricketLeagueCsvFileShouldReturnWhoHadBestStrikeRateWithFive_Wicket() {
+    public void givenCricketLeagueCsvFileShouldReturnWhoHadGreatBowlingAverageWithBestStrikeRate() {
         try {
             leagueAnalyser.loadIplData(CORRECTED_BOWLING_CSV);
-            String sortBasedOnBattingAverage = leagueAnalyser.sortBasedOnStrikeRateWithFiveWkt();
+            String sortBasedOnBattingAverage = leagueAnalyser.sortBasedOnBowlingAverageWithStrikeRate();
             IplBatsmanDTO[] batsmenArray = new Gson().fromJson(sortBasedOnBattingAverage,IplBatsmanDTO[].class);
             Assert.assertEquals("Krishnappa Gowtham",batsmenArray[0].playerName);
         } catch (CSVBuilderException e) {
                 e.printStackTrace();
-        }
-    }
-    @Test
-    public void givenCricketLeagueCsvFileShouldReturnWhoHadBestStrikeRateWithFour_Wicket() {
-        try {
-            leagueAnalyser.loadIplData(CORRECTED_BOWLING_CSV);
-            String sortBasedOnBattingAverage = leagueAnalyser.sortBasedOnStrikeRateWithFouWkt();
-            IplBowlerDTO[] batsmenArray = new Gson().fromJson(sortBasedOnBattingAverage, IplBowlerDTO[].class);
-            Assert.assertEquals("Krishnappa Gowtham",batsmenArray[0].playerName);
-        } catch (CSVBuilderException e) {
-            e.printStackTrace();
         }
     }
 }
